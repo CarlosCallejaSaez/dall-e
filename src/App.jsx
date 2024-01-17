2c8d5d303d735ac44cc3d6c06da08efd8c597d25
CarlosCallejaSaez <carloscallejasez@gmail.com>
2024-01-17 12:41:18 +0100

    yt added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ReactSwitch from "react-switch";
 import Dalle from './components/Dalle/Dalle';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import YoutubeEmbed from './components/YoutubeEmbed/YoutubeEmbed';
 
 
 export const ThemeContext = createContext(null);
@@ -18,6 +19,14 @@ function App() {
       <div>
         <Header/>
         <Dalle/>
+<div className='yt-cont'>
+        <p style={{color:"blue"}}><strong><u>
+        FOR THIS DEMO VERSION, YOU HAVE HERE A DEMO VIDEO
+        </u>
+        </strong>
+      </p>
+      </div>
+      <YoutubeEmbed embedId="MPyZf1S5U_0" />
         <Footer />
         
       
